c7c7c67ea4db17758cc6d241359a6758aad8cf68
YuriMacBook <07yuri@gmail.com>
2022-04-13 13:26:30 -0400

    modularizing client file and adding default comments and name initials

---
+++ client.js
@@ -0,0 +1,27 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function() {
+  const conn = net.createConnection({
+    host: '192.168.2.25', // IP address here,
+    port: 50541// PORT number here,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+  conn.on("data", (data) => {
+    // code that does something when the connection is first established
+    console.log("server says: ", data);
+  });
+
+  conn.on('connect', () => {
+    console.log("Successfully connected to the server");
+    conn.write("Name: YAY");
+  });
+  return conn;
+};
+
+// console.log("Connecting ...");
+// connect();
+
+module.exports = { connect };
---
+++ play.js
@@ -0,0 +1,2 @@
+const { connect } = require('./client');
+connect();
