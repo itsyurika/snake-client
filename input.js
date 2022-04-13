cd9370b1e6dccd2249a945fece82f1307d848d84
YuriMacBook <07yuri@gmail.com>
2022-04-13 15:10:55 -0400

    adding wasd command interacting with the server

--- input.js
+++ input.js
@@ -1,13 +1,29 @@
+
+const { connect } = require('./client');
+let connection;
 // setup interface to handle user input from stdin
 const handleUserInput = function(key) {
   // your code here
   if (key === '\u0003') {
     process.exit();
   }
+  if (key === 'w') {
+    connection.write("Move: up");
+  }
+  if (key === 'a') {
+    connection.write("Move: left");
+  }
+  if (key === 's') {
+    connection.write("Move: down");
+  }
+  if (key === 'd') {
+    connection.write("Move: right");
+  }
   // conn.write("Move:")
 };
 
-const setupInput = function() {
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
--- play.js
+++ play.js
@@ -4,5 +4,5 @@ const { setupInput } = require('./input');
 
 
 console.log("Connecting...");
-connect();
-setupInput();
+let conn = connect();
+setupInput(conn);
